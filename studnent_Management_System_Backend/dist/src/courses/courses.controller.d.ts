0ee72aabebeebdb58d847a588c2fcef54b6eb0e6
Tsigemariam <tsigemariamzewdu20@gmail.com>
2025-01-10 10:57:22 +0300

    fixed css

--- studnent_Management_System_Backend/dist/src/courses/courses.controller.d.ts
+++ studnent_Management_System_Backend/dist/src/courses/courses.controller.d.ts
@@ -4,60 +4,60 @@ export declare class CoursesController {
     private readonly coursesService;
     constructor(coursesService: CoursesService);
     getAllCourses(): Promise<{
-        id: number;
         name: string;
-        code: string;
-        department: import("@prisma/client").$Enums.Department;
         year: number;
+        department: import("@prisma/client").$Enums.Department;
+        id: number;
         createdAt: Date;
         updatedAt: Date;
+        code: string;
         semester: import("@prisma/client").$Enums.Semester;
     }[]>;
     getCourseByName(name: string): Promise<{
         course: {
-            id: number;
             name: string;
-            code: string;
-            department: import("@prisma/client").$Enums.Department;
             year: number;
+            department: import("@prisma/client").$Enums.Department;
+            id: number;
             createdAt: Date;
             updatedAt: Date;
+            code: string;
             semester: import("@prisma/client").$Enums.Semester;
         }[];
     }>;
     getCourseByCode(code: string): Promise<{
         course: {
-            id: number;
             name: string;
-            code: string;
-            department: import("@prisma/client").$Enums.Department;
             year: number;
+            department: import("@prisma/client").$Enums.Department;
+            id: number;
             createdAt: Date;
             updatedAt: Date;
+            code: string;
             semester: import("@prisma/client").$Enums.Semester;
         }[];
     }>;
     getCourseByYear(year: string): Promise<{
         courses: {
-            id: number;
             name: string;
-            code: string;
-            department: import("@prisma/client").$Enums.Department;
             year: number;
+            department: import("@prisma/client").$Enums.Department;
+            id: number;
             createdAt: Date;
             updatedAt: Date;
+            code: string;
             semester: import("@prisma/client").$Enums.Semester;
         }[];
     }>;
     getCourses(year: string, semester: string): Promise<{
         courses: {
-            id: number;
             name: string;
-            code: string;
-            department: import("@prisma/client").$Enums.Department;
             year: number;
+            department: import("@prisma/client").$Enums.Department;
+            id: number;
             createdAt: Date;
             updatedAt: Date;
+            code: string;
             semester: import("@prisma/client").$Enums.Semester;
         }[];
     }>;
--- studnent_Management_System_Backend/dist/src/courses/courses.service.d.ts
+++ studnent_Management_System_Backend/dist/src/courses/courses.service.d.ts
@@ -4,60 +4,60 @@ export declare class CoursesService {
     private readonly prisma;
     constructor(prisma: PrismaService);
     getAll(): Promise<{
-        id: number;
         name: string;
-        code: string;
-        department: import("@prisma/client").$Enums.Department;
         year: number;
+        department: import("@prisma/client").$Enums.Department;
+        id: number;
         createdAt: Date;
         updatedAt: Date;
+        code: string;
         semester: import("@prisma/client").$Enums.Semester;
     }[]>;
     getByName(name: string): Promise<{
         course: {
-            id: number;
             name: string;
-            code: string;
-            department: import("@prisma/client").$Enums.Department;
             year: number;
+            department: import("@prisma/client").$Enums.Department;
+            id: number;
             createdAt: Date;
             updatedAt: Date;
+            code: string;
             semester: import("@prisma/client").$Enums.Semester;
         }[];
     }>;
     getByCode(code: string): Promise<{
         course: {
-            id: number;
             name: string;
-            code: string;
-            department: import("@prisma/client").$Enums.Department;
             year: number;
+            department: import("@prisma/client").$Enums.Department;
+            id: number;
             createdAt: Date;
             updatedAt: Date;
+            code: string;
             semester: import("@prisma/client").$Enums.Semester;
         }[];
     }>;
     getByYear(year: number): Promise<{
         courses: {
-            id: number;
             name: string;
-            code: string;
-            department: import("@prisma/client").$Enums.Department;
             year: number;
+            department: import("@prisma/client").$Enums.Department;
+            id: number;
             createdAt: Date;
             updatedAt: Date;
+            code: string;
             semester: import("@prisma/client").$Enums.Semester;
         }[];
     }>;
     getByYearAndSemester(year: number, semester: Semester): Promise<{
         courses: {
-            id: number;
             name: string;
-            code: string;
-            department: import("@prisma/client").$Enums.Department;
             year: number;
+            department: import("@prisma/client").$Enums.Department;
+            id: number;
             createdAt: Date;
             updatedAt: Date;
+            code: string;
             semester: import("@prisma/client").$Enums.Semester;
         }[];
     }>;
